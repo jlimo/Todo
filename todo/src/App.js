ff5ff974548e40a7bc4fbdcd4d3a3f8ff8f6ba9e
jolean lim <joleanflima@outlook.com>
2023-10-30 20:30:43 -0400

    first commit setting up frontend

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//first thing is to create the frontend layout so we know what is happening
+export default function App() {
+  return  (
+    <>
+  <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" id="item" />
     </div>
-  );
+    <button className='btn'> Add</button>
+  </form>
+  <h1 className='header'>Todo List</h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox" />
+        Item 1
+      </label>
+      <label>
+        <input type="checkbox" />
+        Item 2
+      </label>
+      <button className="btn btn-Danger">Delete</button>
+    </li>
+  </ul>
+  </>
+  )
 }
-
-export default App;
