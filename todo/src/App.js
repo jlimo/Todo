2810ed0aab68e9bcb2409c4221d5458598ce3998
jolean lim <joleanflima@outlook.com>
2023-10-30 21:59:55 -0400

    giving the props unique keys

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,28 +1,42 @@
 import './App.css';
+import { useState } from 'react';
 //first thing is to create the frontend layout so we know what is happening
 export default function App() {
+  const [newItem, SetNewItem]= useState("")
+  const [todos, SetTodos]= useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    SetTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+  }
   return  (
     <>
-  <form className="new-item-form">
+  <form onSubmit={handleSubmit}className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item" />
+      <input value={newItem} 
+      onChange={e => SetNewItem(e.target.value)}
+       type="text" id="item" />
     </div>
     <button className='btn'> Add</button>
   </form>
   <h1 className='header'>Todo List</h1>
   <ul className="list">
-    <li>
-      <label>
-        <input type="checkbox" />
-        Item 1
-      </label>
-      <label>
-        <input type="checkbox" />
-        Item 2
+    {todos.map(todo => {
+      return <li>
+    <label>
+      <input type="checkbox" checked={todo.completed} />
+      {todo.title}
       </label>
       <button className="btn btn-Danger">Delete</button>
     </li>
+    })}
   </ul>
   </>
   )
